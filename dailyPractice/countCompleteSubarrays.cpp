e0bb3cb7621518a1445e883e7e2d5d28ed94ae07
RicardosRoss <ricardosrosswork@gmail.com>
2025-04-26 03:32:22 +0000

    feat:250425&&250424

--- dailyPractice/countCompleteSubarrays.cpp
+++ dailyPractice/countCompleteSubarrays.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 using namespace std;
+
 bool isEqual(unordered_map<int, int> &mp, unordered_map<int, int> &curMp)
 {
 
@@ -46,9 +48,37 @@ int countCompleteSubarrays(vector<int> &nums)
     }
     return cnt;
 }
+long long countInterestingSubarrays(vector<int> &nums, int modulo, int k)
+{
+    long long res = 0;
+    vector<int> prefixSum = {0}; // 初始化前缀和数组，避免在空数组上调用 back()
+    vector<int> cnt(min((int)nums.size() + 1, modulo), 0);
+    for (int num : nums)
+    {
+        prefixSum.push_back(prefixSum.back() + (num % modulo == k ? 1 : 0));
+    }
+    for (int s : prefixSum)
+    {
+        if (s > k)
+        {
+            res += cnt[(s - k) % modulo];
+        }
+        cnt[s % modulo]++;
+    }
+    return res;
+}
+void testCountCompleteSubarrays()
+{
+    vector<int> nums = {3, 1, 9, 6};
+    cout << countCompleteSubarrays(nums) << endl;
+}
+void testCountInterestingSubarrays()
+{
+    vector<int> nums = {3, 1, 9, 6};
+    cout << countInterestingSubarrays(nums, 3, 0) << endl;
+}
 int main()
 {
-    vector<int> nums = {5, 5, 5, 5};
-    cout << countCompleteSubarrays(nums) << endl;
+    testCountInterestingSubarrays();
     return 0;
 }
